161b6f43e8a954f5f6e3fd48d5b94219d7648112
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-07-08 16:50:10 +0300

    Add levenshtein_distance

--- population.py
+++ population.py
@@ -1,11 +1,23 @@
-import creature 
+import itertools
 import numpy as np
+import Levenshtein
+
+import creature
+
 
 class Population:
     def __init__(self, pop_size, gene_count):
         self.creatures = [creature.Creature(
                           gene_count=gene_count) 
                           for i in range(pop_size)]
+
+    def levenshtein_distance(self):
+        pairwise_distances = []
+        for dna1, dna2 in itertools.combinations([creature.dna for creature in self.creatures], 2):
+            distance = Levenshtein.distance(np.array(dna1).flatten(), np.array(dna2).flatten())
+            pairwise_distances.append(distance)
+
+        return np.mean(pairwise_distances)
 
     @staticmethod
     def get_fitness_map(fits):
